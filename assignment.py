48a00e10bf4d10ca5cfacf31aed41e47c0d5eb03
justin sanders <justin_sanders1@brown.edu>
2020-11-26 12:37:28 -0800

    first draft of model on CIFAR

--- assignment.py
+++ assignment.py
@@ -18,70 +18,132 @@ class Model(tf.keras.Model):
         self.num_classes = num_classes
         self.num_examples = num_examples
 
+        self.similarity = tf.keras.losses.CosineSimilarity(axis = 1)
+
         self.batch_size = 100
         self.loss_list = []
+        self.acc_list = []
 
         self.learning_rate = 0.001
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
+        self.normalize = tf.keras.layers.BatchNormalization()
+
+        self.conv_1 = tf.keras.layers.Conv2D(16, 5, strides = (2,2), padding='SAME', activation=tf.keras.layers.LeakyReLU(alpha=0.2), kernel_initializer=tf.random_normal_initializer(stddev=0.1), activity_regularizer=)
+        self.pool_1 = tf.keras.layers.MaxPool2D((3, 3), strides=(2,2))
+        self.conv_2 = tf.keras.layers.Conv2D(32, 3, strides = (2,2), padding='SAME', activation=tf.keras.layers.LeakyReLU(alpha=0.2), kernel_initializer=tf.random_normal_initializer(stddev=0.1))
+        self.pool_2 = tf.keras.layers.MaxPool2D((2, 2), strides=(2,2))
+        self.conv_3 = tf.keras.layers.Conv2D(32, 3, strides = (1,1), padding='SAME', activation=tf.keras.layers.LeakyReLU(alpha=0.2), kernel_initializer=tf.random_normal_initializer(stddev=0.1))
+        self.lstm = tf.keras.layers.LSTM(128, return_sequences=True, return_state=True)
+        self.distance = tf.keras.layers.Dense(2)
 
     def call(self, inputs, examples):
         """
         Runs a forward pass on an input batch of images examples and test images
         """
-        print(inputs.shape)
-        print(examples.shape)
-        print(" ")
-        pass
+        examples = self.conv_1(examples)
+        examples = self.normalize(examples)
+        examples = self.pool_1(examples)
+        examples = self.conv_2(examples)
+        examples = self.normalize(examples)
+        examples = self.pool_2(examples)
+        examples = self.conv_3(examples)
+        examples = self.normalize(examples)
+        examples = tf.reshape(examples, [self.num_examples, -1])
+        _, merged_examples, _ = self.lstm(tf.expand_dims(examples, axis=0), initial_state=None)
+
+        inputs = self.conv_1(inputs)
+        inputs = self.normalize(inputs)
+        inputs = self.pool_1(inputs)
+        inputs = self.conv_2(inputs)
+        inputs = self.normalize(inputs)
+        inputs = self.pool_2(inputs)
+        inputs = self.conv_3(inputs)
+        inputs = self.normalize(inputs)
+        inputs = tf.reshape(inputs, [self.batch_size, -1])
+
+        merged_examples = tf.stack([merged_examples[0]] * self.batch_size)
+
+        dist = (-tf.keras.losses.cosine_similarity(merged_examples, inputs) + 1) / 2
+
+        #dist = -self.similarity(merged_examples, inputs)
+
+        #print(dist.shape)
+        return dist
+
+        #combined = tf.concat([merged_examples, inputs], 1)
+
+        #logits = self.distance(combined)
+        #return logits
+
+
 
     def loss(self, logits, labels):
         """
         Calculates the model cross-entropy loss after one forward pass.
         """
-        pass
+        #return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, logits))
+        labels = tf.argmax(labels, 1)
+        return tf.reduce_sum(tf.square(logits - tf.cast(labels, tf.float32)))
 
     def accuracy(self, logits, labels):
         """
         Calculates the model's prediction accuracy by comparing
         logits to correct labels
         """
-        pass
+        labels = tf.cast(tf.argmax(labels, 1), tf.float32)
+        logits = tf.cast(tf.math.greater_equal(logits, 0.5), tf.float32)
+        correct_predictions = tf.equal(labels, logits)
+        return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
-def train(model, examples, train_inputs, train_labels):
+def train(model, examples):
     '''
     Trains the model on all of the inputs and labels for one epoch.
     '''
-    losses = []
-    indices = tf.random.shuffle(range(0, len(train_labels)))
-    train_inputs = tf.gather(train_inputs, indices)
-    train_inputs = tf.image.random_flip_left_right(train_inputs)
-    train_labels = tf.gather(train_labels, indices)
+    #indices = tf.random.shuffle(range(0, len(train_labels)))
+    #train_inputs = tf.gather(train_inputs, indices)
+    #train_inputs = tf.image.random_flip_left_right(train_inputs)
+    #train_labels = tf.gather(train_labels, indices)
 
-    for ii in range(0, len(train_labels), model.batch_size):
+    for ii in range(len(examples)):
+        example_indices = np.random.choice(len(examples[ii]), model.num_examples)
+        batch_examples = examples[ii][example_indices]
+
+        batch_pos_indices = np.random.choice(len(examples[ii]), int(model.batch_size/2))
+        batch_pos_inputs = examples[ii][batch_pos_indices]
+        batch_pos_labels = np.zeros((int(model.batch_size/2), 2))
+        batch_pos_labels[:,1] = 1
+
+        batch_neg_indices = (np.random.choice(len(examples), int(model.batch_size/2)) , np.random.choice(len(examples[ii]), int(model.batch_size/2)))
+        batch_neg_inputs = examples[batch_neg_indices]
+        batch_neg_labels = np.zeros((int(model.batch_size/2), 2))
+        batch_neg_labels[:,0] = 1
+
+        batch_inputs = np.concatenate((batch_pos_inputs, batch_neg_inputs))
+        batch_labels = np.concatenate((batch_pos_labels, batch_neg_labels))
+
+        with tf.GradientTape() as tape:
+            logits = model.call(batch_inputs, batch_examples)
+            loss = model.loss(logits, batch_labels)
+            model.loss_list.append(loss.numpy())
+
+            acc = model.accuracy(logits, batch_labels)
+            model.acc_list.append(acc.numpy())
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+def test(model, examples, test_inputs, test_labels):
+    """
+    Tests the model on the test inputs and labels.
+    """
+    for ii in range(len(examples)):
         batch_inputs = train_inputs[ii : ii + model.batch_size]
         batch_labels = train_labels[ii : ii + model.batch_size]
 
         example_label = random.randint(0,model.num_classes-1)
         example_indices = np.random.choice(len(examples[example_label]), model.num_examples)
         batch_examples = examples[example_label][example_indices]
-
-        print(batch_labels.shape)
-        model.call(batch_inputs, batch_examples)
-
-        #with tf.GradientTape() as tape:
-        #    predictions =
-        #    loss =
-        #    model.loss_list.append(loss)
-
-        #gradients = tape.gradient(loss, model.trainable_variables)
-        #optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-
-def test(model, test_inputs, test_labels):
-    """
-    Tests the model on the test inputs and labels.
-    """
-
 
 
 def visualize_loss(losses):
@@ -96,6 +158,21 @@ def visualize_loss(losses):
     plt.xlabel('Batch')
     plt.ylabel('Loss')
     plt.savefig("episode_loss.jpg")
+    plt.clf()
+
+def visualize_acc(losses):
+    """
+    Uses Matplotlib to visualize the losses of our model.
+    :param losses: list of loss data stored from train. Can use the model's loss_list
+    field
+    """
+    x = [i for i in range(len(losses))]
+    plt.plot(x, losses)
+    plt.title('Train Accuracy per batch')
+    plt.xlabel('Batch')
+    plt.ylabel('Acc')
+    plt.savefig("episode_acc.jpg")
+    plt.clf()
 
 
 def preprocess():
@@ -111,20 +188,24 @@ def preprocess():
     for ii in range(len(train_labels)):
         examples[train_labels[ii]].append(train_data[ii])
 
-    examples = np.asarray(examples)
-    return examples, train_data, train_labels
+    examples = np.asarray(examples).astype(np.float32)/255
+    return examples
 
 
 def main():
     #get train data
-    examples, train_data, train_labels = preprocess()
+    examples = preprocess()
     print(examples.shape)
-    print(train_data.shape)
-    print(train_labels.shape)
 
     model = Model(100, 5)
 
-    train(model, examples, train_data, train_labels)
+    for epoch in range(1000):
+        if epoch % 10 == 0:
+            visualize_loss(model.loss_list)
+            visualize_acc(model.acc_list)
+        print(epoch)
+        #print(model.acc_list)
+        train(model, examples)
 
 
 if __name__ == '__main__':
